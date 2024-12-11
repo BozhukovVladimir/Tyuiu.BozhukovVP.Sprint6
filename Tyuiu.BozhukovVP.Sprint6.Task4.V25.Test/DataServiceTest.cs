ab3e33da683759b08f7d1527194d99900d901d6b
Vladimir Bozhukov <bozhukovvladimir@yandex.ru>
2024-12-11 19:43:10 +0500

    Task4

---
+++ Tyuiu.BozhukovVP.Sprint6.Task4.V25.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.BozhukovVP.Sprint6.Task4.V25.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.BozhukovVP.Sprint6.Task4.V25/FormMain.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.BozhukovVP.Sprint6.Task4.V25
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
