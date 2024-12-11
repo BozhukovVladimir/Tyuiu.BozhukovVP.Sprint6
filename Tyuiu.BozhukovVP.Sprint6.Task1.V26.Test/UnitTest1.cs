234302c0df2188c5ba8659c43e380d803a6aef25
Vladimir Bozhukov <bozhukovvladimir@yandex.ru>
2024-12-11 11:08:08 +0500

    Task1

---
+++ Tyuiu.BozhukovVP.Sprint6.Task1.V26.Test/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Tyuiu.BozhukovVP.Sprint6.Task1.V26.Lib;
+namespace Tyuiu.BozhukovVP.Sprint6.Task1.V26.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
