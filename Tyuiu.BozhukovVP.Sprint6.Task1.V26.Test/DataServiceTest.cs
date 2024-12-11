fd744e2a0441c7417b286b459986b6dd38895b55
Vladimir Bozhukov <bozhukovvladimir@yandex.ru>
2024-12-11 16:17:24 +0500

    Task1

--- Tyuiu.BozhukovVP.Sprint6.Task1.V26.Test/DataServiceTest.cs
+++ Tyuiu.BozhukovVP.Sprint6.Task1.V26.Test/DataServiceTest.cs
@@ -2,10 +2,10 @@ using Tyuiu.BozhukovVP.Sprint6.Task1.V26.Lib;
 namespace Tyuiu.BozhukovVP.Sprint6.Task1.V26.Test
 {
     [TestClass]
-    public class UnitTest1
+    public class DataServiceTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ()
         {
         }
     }
